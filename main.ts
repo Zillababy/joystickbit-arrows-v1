8a49e00241ae2e0d01a7b92d48e0ab2de5c9606a
Zillababy <bcinelli69@gmail.com>
2021-07-18 22:34:25 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,49 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == East) {
+        basic.showArrow(ArrowNames.East)
+        DFRobotMaqueenPlus.mototStop(Motors.M2)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+    } else if (receivedString == South) {
+        basic.showArrow(ArrowNames.South)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 50)
+    } else if (receivedString == West) {
+        basic.showArrow(ArrowNames.West)
+        DFRobotMaqueenPlus.mototStop(Motors.M1)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+    } else if (receivedString == North) {
+        basic.showArrow(ArrowNames.North)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
+    } else {
+        basic.showIcon(IconNames.Happy)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    }
+})
+let North = ""
+let West = ""
+let South = ""
+let East = ""
 joystickbit.initJoystickBit()
-radio.setGroup(1)
-basic.showIcon(IconNames.Heart)
-music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+radio.setGroup(69)
 basic.forever(function () {
-    radio.sendNumber(1)
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) <= 200) {
-        radio.sendString("R")
+        radio.sendString("" + (East))
         joystickbit.Vibration_Motor(100)
         basic.showArrow(ArrowNames.East)
     }
     if (joystickbit.getRockerValue(joystickbit.rockerType.Y) <= 200) {
-        radio.sendString("B")
+        radio.sendString("" + (South))
         joystickbit.Vibration_Motor(100)
         basic.showArrow(ArrowNames.South)
     }
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) >= 800) {
-        radio.sendString("L")
+        radio.sendString("" + (West))
         joystickbit.Vibration_Motor(100)
         basic.showArrow(ArrowNames.West)
     }
     if (joystickbit.getRockerValue(joystickbit.rockerType.Y) >= 800) {
-        radio.sendString("F")
+        radio.sendString("" + (North))
         joystickbit.Vibration_Motor(100)
         basic.showArrow(ArrowNames.North)
     }
+    basic.clearScreen()
 })
