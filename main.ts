8f3a187376834fd993650a7069a498d4033c0a5b
Zillababy <bcinelli69@gmail.com>
2021-07-18 21:30:09 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+joystickbit.initJoystickBit()
+radio.setGroup(1)
+basic.showIcon(IconNames.Heart)
+music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) <= 200) {
+        radio.sendString("R")
+        joystickbit.Vibration_Motor(100)
+        basic.showArrow(ArrowNames.East)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) <= 200) {
+        radio.sendString("B")
+        joystickbit.Vibration_Motor(100)
+        basic.showArrow(ArrowNames.South)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) >= 800) {
+        radio.sendString("L")
+        joystickbit.Vibration_Motor(100)
+        basic.showArrow(ArrowNames.West)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) >= 800) {
+        radio.sendString("F")
+        joystickbit.Vibration_Motor(100)
+        basic.showArrow(ArrowNames.North)
+    }
+})
